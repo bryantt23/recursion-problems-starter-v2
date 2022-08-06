81ddf471ddf34712942bf4fff02980c2e2b59f7b
bryant tunbutr <bryantt23@users.noreply.github.com>
2022-08-05 21:34:17 -0700

    pass test

--- starter/problems/17-bonus-permutations.js
+++ starter/problems/17-bonus-permutations.js
@@ -13,6 +13,30 @@ permutations([1, 2, 3]) // [[1, 2, 3], [1, 3, 2],
 ***********************************************************************/
 
 // your code here
+function permutations(arr) {
+  let n;
+  let res = [];
+  n = arr.length;
+  let used = Array(n);
+  build(arr, [], used, res, n);
+  return res;
+}
+
+function build(arr, list, used, res, n) {
+  //bc
+  if (list.length === n) {
+    res.push(list);
+    return;
+  }
+
+  for (let i = 0; i < n; i++) {
+    if (!used[i]) {
+      used[i] = true;
+      build(arr, [...list, arr[i]], used, res, n);
+      used[i] = false;
+    }
+  }
+}
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
